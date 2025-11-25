cdc2b98057b880c8a8763ad429ceda1f13558838
LudwigPosn1 <ludwigpoisson2105@gmail.com>
2025-11-25 20:01:27 +0100

    modifications admin

--- admin.php
+++ admin.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['users']) || $_SESSION['role_id'] != 1) { //Vérifie si l'u
 // A la fin de ce if, on sait si l'utilisateru est connecté et est un administrateur
 $sql = "SELECT * FROM users";
 $stmt = $pdo->query($sql); // Execute la requête de la même manière que prepare + execute saud qu'ici nous sommes en admisnistrateur donc c'est plus sécurisé de faire comme ça
-
+$users = $ stmt -> fetchAll(PDO::FETCH_ASSOC);// récupére toutes les lignes et les mets sous forme de tableaux
 
 
 
@@ -22,10 +22,15 @@ $stmt = $pdo->query($sql); // Execute la requête de la même manière que prepa
 		<meta name="viemport" content="width=device-width, initial-scale=1.0">
 		<title>Espace Administrateur</title>
     </head>
-
-
     <body>
         <h1>Bonjour, <?php echo htmlspecialchars($_SESSION['user_nom']) ?></h1>
+        <h2>Liste des utlisateurs</h2>
+        <table border="1">
+            <tr>
+                <td><?php htmlspecialchars($user['nom'])?></td>
+                <td><?php htmlspecialchars($user['email'])?></td>
+            </tr>
+            <?php endforeach; ?>
         <a href="logout.php">Se déconnecter</a>
     </body>
 <html>
