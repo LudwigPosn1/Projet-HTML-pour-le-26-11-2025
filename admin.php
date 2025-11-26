8c95be0f1fbbc858c378078f23ba025980b4bee8
LudwigPosn1 <ludwigpoisson2105@gmail.com>
2025-11-26 22:29:55 +0100

    ajout du CSS pour l'administrateur

--- admin.php
+++ admin.php
@@ -11,68 +11,75 @@ $users = getAllUsers($pdo);
 ?>
 
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Espace administrateur</title>
+    <link rel="stylesheet" href="style-admin.css">
+</head>
 <html>
 <body>
-    <p><a href="tableau.php">Retour à mon compte</a></p>
-    <h1> — Accès ADMINISTRATEUR </h1>
+    <p class="back"><a href="tableau.php">Retour à mon compte</a></p>
+    <h1 class="titre1"> Accès ADMINISTRATEUR </h1>
 
-    <table border="1">
-        <tr>
-            <th> ID </th>
-            <th> NOM </th>
-            <th> EMAIL </th>
-            <th> ROLE </th>
-            <th> CHANGER DE ROLE </th>
-            <th> SUPPRIMER </th>
+    <table class="list_user" border="1">
+        <tr class ="ligne1">
+            <th class="colonne1"> ID </th>
+            <th class="colonne2"> NOM </th>
+            <th class="colonne3"> EMAIL </th>
+            <th class="colonne4"> ROLE </th>
+            <th class="colonne5"> CHANGER DE ROLE </th>
+            <th class="colonne6"> SUPPRIMER </th>
         </tr>
 
         <?php foreach ($users as $u): ?>
-        <tr>
-            <td> <?= $u['id'] ?></td>
-            <td> <?= $u['nom'] ?></td>
-            <td> <?= $u['email'] ?></td>
-            <td> <?= $u['role_name'] ?></td>
-            <td>
+        <tr class ="ligne2">
+            <td class="colonne1.1"> <?= $u['id'] ?></td>
+            <td class="colonne1.2"> <?= $u['nom'] ?></td>
+            <td class="colonne1.3"> <?= $u['email'] ?></td>
+            <td class="colonne1.4"> <?= $u['role_name'] ?></td>
+            <td class="colonne1.5">
                 <form method="POST" action="update_role.php">
                     <input type="hidden" name="user_id" value="<?= $u['id'] ?>">
-                    <select name="role_id">
-                        <option value="1"> USER </option>
-                        <option value="2"> ADMINISTRATEUR </option>
+                    <select class="choix_role" name="role_id">
+                        <option class="option1" value="1"> USER </option>
+                        <option class="option2" value="2"> ADMINISTRATEUR </option>
                     </select>
-                    <button type="submit"> Modifier </button>
+                    <button class="modify_button" type="submit"> Modifier </button>
                 </form>
             </td>
-            <td> 
+            <td class="colonne1.6"> 
                 <form method="POST" action="delete_user.php" onsubmit="return confirm('Êtes-vous certain de vouloir supprimer ce compte ? Les données seront perdues.');">
                     <input type="hidden" name="user_id" value="<?=$u['id'] ?>">
-                    <button type="submit" class="btn btn-danger">Supprimer le compte</button>
+                    <button class="delete_bouton" type="submit" class="btn btn-danger">Supprimer le compte</button>
                 </form>
             </td>
         </tr>
         <?php endforeach; ?>
     </table>
-<h2>Ajouter un utilisateur</h2>
+<h2 class="sous_titres1">Ajouter un utilisateur</h2>
 
 <form method="POST" action="add_user.php">
-    <label>Nom :</label>
-    <input type="text" name="nom" required>
+    <label class="name">Nom :</label>
+    <input class="name_for_create" type="text" name="nom" required>
 <br>
-    <label>Email :</label>
-    <input type="email" name="email" required>
+    <label class="email">Email :</label>
+    <input class="email_for_create" type="email" name="email" required>
 <br>
-    <label>Adresse :</label>
-    <input type="text" name="adresse" required>
+    <label class="adress">Adresse :</label>
+    <input class="adress_for_create" type="text" name="adresse" required>
 <br>
-    <label>Mot de passe :</label>
-    <input type="password" name="password" required>
+    <label class="password">Mot de passe :</label>
+    <input class="password_for_create" type="password" name="password" required>
 <br>
     <label>Rôle :</label>
-    <select name="role_id">
-        <option value="1">Utilisateur</option>
-        <option value="2">Administrateur</option>
+    <select class="type_role" name="role_id">
+        <option class="option3" value="1">Utilisateur</option>
+        <option class="option4" value="2">Administrateur</option>
     </select>
 <br>
-    <button type="submit">Créer l'utilisateur</button>
+    <button class="create_validation" type="submit">Créer l'utilisateur</button>
 </form>
 
 </body>
